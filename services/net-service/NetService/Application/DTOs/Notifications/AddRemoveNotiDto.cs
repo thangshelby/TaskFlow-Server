dae73b8aa4b67fcd630ac19e44e4fe50a7f2fdc7
vudinhan2525 <vudinhan000@gmail.com>
2025-03-29 16:05:34 +0700

    Fix envoy

--- services/net-service/NetService/Application/DTOs/Notifications/AddRemoveNotiDto.cs
+++
@@ -1,7 +0,0 @@
-namespace NetService.Application.DTOs.Notifications;
-
-public class AddRemoveNotificationDto
-{
-    public int UserId { get; set; }
-    public int NotificationId { get; set; }
-}
--- services/net-service/NetService/Application/DTOs/Notifications/NewNotificationDto.cs
+++
@@ -1,7 +0,0 @@
-namespace NetService.Application.DTOs.Notifications;
-
-public class NewNotificationDto
-{
-    public required string NotiHeader { get; set; }
-    public required string NotiContent { get; set; }
-}
