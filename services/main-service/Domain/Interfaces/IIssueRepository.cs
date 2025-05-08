0ecf23eef928b4658e5dff0fd2e2f3e5e1846120
vudinhan2525 <vudinhan000@gmail.com>
2025-05-08 16:01:23 +0700

    Fix update issue

--- services/main-service/Domain/Interfaces/IIssueRepository.cs
+++ services/main-service/Domain/Interfaces/IIssueRepository.cs
@@ -1,10 +1,11 @@
 using MainService.Domain.Entities;
+using MainService.Domain.Enums;
 namespace MainService.Domain.Interfaces;
 public interface IIssueRepository
 {
     Task<IssueDomain> CreateIssue(IssueDomain issue);
     Task<IssueDomain> GetIssue(string id);
-    Task<IssueDomain> UpdateIssue(IssueDomain issue);
+    Task<IssueDomain> UpdateIssue(UpdateIssueParams issue);
     Task DeleteIssue(string id);
     Task<(List<IssueDomain> Issues, int TotalCount)> ListIssues(GetIssuesParams param);
 }
@@ -27,4 +28,21 @@ public class GetIssuesParams
     public string? Keyword;
     public int Page;
     public int Limit;
+}
+public class UpdateIssueParams
+{
+    public required string IssueId;
+    public string? Title;
+    public string? ProjectId;
+    public string? SprintId;
+    public string? AssigneeId;
+    public string? Description;
+    public string? Summary;
+    public int? StoryPoint;
+    public string? ReporterId;
+    public string? Status;
+    public string? ParentId;
+    public IssueType? Type;
+    public IssuePriority? Priority;
+    public List<string>? Attachments; 
 }
